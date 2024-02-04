e14ea909e5dda176c44666c6d31951038e5c40c6
matehaxor03 <matehaxor03>
2024-02-05 08:36:17 +1100

    create seperate internal create user function

--- host_installer/HostInstaller.go
+++ host_installer/HostInstaller.go
@@ -34,6 +34,90 @@ func NewHostInstaller(users_directory []string, number_of_users uint64, userid_o
 	getUserIdOffset := func() uint64 {
 		return this_userid_offset
 	}
+
+	create_user := func(username string, absolute_home_directory_path []string, primary_user_id uint64, primary_group_id uint64) []error {
+		user_directory, user_directory_errors := host_client.AbsoluteDirectory(absolute_home_directory_path)
+		if user_directory_errors != nil {
+			return user_directory_errors
+		}
+
+		if !user_directory.Exists() {
+			user_directory_create_errors := user_directory.Create()
+			if user_directory_create_errors != nil {
+				return user_directory_create_errors
+			}
+		}
+
+		host_user, host_user_errors := host_client.HostUser(username) 
+
+		if host_user_errors != nil {
+			return host_user_errors
+		}
+
+		exists, exists_error := host_user.Exists()
+		if exists_error != nil {
+			return exists_error
+		}
+
+		if !*exists {
+			create_errors := host_user.Create()
+			if create_errors != nil {
+				return create_errors
+			}
+		}
+
+		set_unique_id_errors := host_user.SetUniqueId(primary_user_id)
+		if set_unique_id_errors != nil {
+			return set_unique_id_errors
+		}
+
+		group, group_errors := host_client.Group(username) 
+
+		if group_errors != nil {
+			return group_errors
+		}
+
+		group_exists, group_exists_errors := group.Exists()
+		if group_exists_errors != nil {
+			return group_exists_errors
+		}
+
+		if !*group_exists {
+			group_create_errors := group.Create()
+			if group_create_errors != nil {
+				return group_create_errors
+			}
+		}
+
+		set_group_unique_id_errors := group.SetUniqueId(primary_group_id)
+		if set_group_unique_id_errors != nil {
+			return set_group_unique_id_errors
+		}
+
+		add_user_to_group_errors := group.AddUser(*host_user)
+		if add_user_to_group_errors != nil {
+			return add_user_to_group_errors
+		}
+
+		set_user_primary_group_id_errors := host_user.SetPrimaryGroupId(primary_group_id)
+		if set_user_primary_group_id_errors != nil {
+			return set_user_primary_group_id_errors
+		}
+
+		create_home_directory_errors := host_user.CreateHomeDirectoryAbsoluteDirectory(*user_directory)
+
+		if create_home_directory_errors != nil {
+			return create_home_directory_errors
+		}
+
+		set_user_directory_errors := user_directory.SetOwnerRecursive(*host_user, *group)
+		
+		if set_user_directory_errors != nil {
+			return set_user_directory_errors
+		}
+
+		return nil
+	}
 	
 	install := func() ([]error) {
 		users_directory_parts := getUsersDirectory()
@@ -54,89 +138,14 @@ func NewHostInstaller(users_directory []string, number_of_users uint64, userid_o
 
 		max_user_id := temp_this_userid_offset + temp_this_number_of_users
 
-		for i := temp_this_userid_offset; i < max_user_id; i++ {
+		for i := temp_this_userid_offset ; i < max_user_id; i++ {
 			current_username := "holisticxyz_b" + strconv.FormatUint(i, 10) + "_"
 			var current_username_directory []string
 			current_username_directory = append(current_username_directory, users_directory.GetPath()...)
 			current_username_directory = append(current_username_directory, current_username)
-			user_directory, user_directory_errors := host_client.AbsoluteDirectory(current_username_directory)
-			if user_directory_errors != nil {
-				return user_directory_errors
-			}
-
-			if !user_directory.Exists() {
-				user_directory_create_errors := user_directory.Create()
-				if user_directory_create_errors != nil {
-					return user_directory_create_errors
-				}
-			}
-
-			host_user, host_user_errors := host_client.HostUser(current_username) 
-
-			if host_user_errors != nil {
-				return host_user_errors
-			}
-
-			exists, exists_error := host_user.Exists()
-			if exists_error != nil {
-				return exists_error
-			}
-
-			if !*exists {
-				create_errors := host_user.Create()
-				if create_errors != nil {
-					return create_errors
-				}
-			}
-
-			set_unique_id_errors := host_user.SetUniqueId(i)
-			if set_unique_id_errors != nil {
-				return set_unique_id_errors
-			}
-
-			group, group_errors := host_client.Group(current_username) 
-
-			if group_errors != nil {
-				return group_errors
-			}
-
-			group_exists, group_exists_errors := group.Exists()
-			if group_exists_errors != nil {
-				return group_exists_errors
-			}
-
-			if !*group_exists {
-				group_create_errors := group.Create()
-				if group_create_errors != nil {
-					return group_create_errors
-				}
-			}
-
-			set_group_unique_id_errors := group.SetUniqueId(i)
-			if set_group_unique_id_errors != nil {
-				return set_group_unique_id_errors
-			}
-
-			add_user_to_group_errors := group.AddUser(*host_user)
-			if add_user_to_group_errors != nil {
-				return add_user_to_group_errors
-			}
-
-			set_user_primary_group_id_errors := host_user.SetPrimaryGroupId(i)
-			if set_user_primary_group_id_errors != nil {
-				return set_user_primary_group_id_errors
-			}
-
-			create_home_directory_errors := host_user.CreateHomeDirectoryAbsoluteDirectory(*user_directory)
-
-			if create_home_directory_errors != nil {
-				return create_home_directory_errors
-			}
-
-			set_user_directory_errors := user_directory.SetOwnerRecursive(*host_user, *group)
-			
-			if set_user_directory_errors != nil {
-				return set_user_directory_errors
+			create_user_errors := create_user(current_username, current_username_directory, i, i)
+			if create_user_errors != nil {
+				return create_user_errors
 			}
 		}
 
